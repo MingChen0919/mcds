d7da59eb3a1ba1374cba981ede2ab2f37472cf62
mingchen0919 <ming@cryoocyte.com>
2019-05-20 23:09:46 -0400

    update

---
+++ create_output_dir.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from datetime import datetime
+
+def create_output_dir(prefix='', suffix=''):
+	"""
+	Create a directory named prefix + current time + suffix for storing current analysis results.
+	Exp directory name: prefix_201905211430_suffix.
+
+	:param prefix: str,
+	:param suffix:
+	:return:
+	"""
+	return Path(prefix + datetime.now().strftime('%Y%m%d%H%M') + suffix).mkdir(parents=True, exist_ok=True)
+
+if __name__ == '__main__':
+    create_output_dir('salar_')
