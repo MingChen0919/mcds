4d69ff0247019dc913f189b83136ad31b61be742
mingchen0919 <ming@cryoocyte.com>
2019-05-21 10:27:01 -0400

    update

--- mcds.py
+++ mcds.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 from datetime import datetime
 
+
 def create_output_dir(prefix='', suffix=''):
 	"""
 	Create a directory named prefix + current time + suffix for storing current analysis results.
 	Exp directory name: prefix_201905211430_suffix.
-
 	:param prefix: str,
 	:param suffix:
 	:return:
@@ -13,5 +13,5 @@ def create_output_dir(prefix='', suffix=''):
 
 	dir_path = Path(prefix + datetime.now().strftime('%Y%m%d%H%M') + suffix)
 	dir_path.mkdir(parents=True, exist_ok=True)
-
+	print(str(dir_path))
 	return str(dir_path)
